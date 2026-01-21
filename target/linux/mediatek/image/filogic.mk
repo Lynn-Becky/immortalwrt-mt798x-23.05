e10abe8085ca98169e378a09639e74ca4bbdef7c
Lynn-Becky <60915579+Lynn-Becky@users.noreply.github.com>
2026-01-22 03:48:47 +0800

    mediatek: filogic: add support for CLT R30B1
    Port CLT R30B1 device from kernel 5.4 branch to 6.6 branch.
    
    - Add mt7981b-clt-r30b1.dtsi (base device tree)
    - Add mt7981b-clt-r30b1.dts (64MB UBI partition)
    - Add mt7981b-clt-r30b1-112m.dts (112MB UBI partition)
    - Add device definitions in filogic.mk
    - Update defconfig for new device names
    
    Hardware specs:
    - SoC: MediaTek MT7981B
    - RAM: 256MB
    - Flash: SPI-NAND
    - WiFi: MT7915E (2.4G + 5G)
    - Ethernet: 3x LAN (via MT7531) + 1x WAN (internal GbE PHY)
    - LEDs: power, wan, mesh, wifi, internet, ctrl
    - Buttons: reset, mesh

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -509,6 +509,43 @@ define Device/cetron_ct3003
 endef
 TARGET_DEVICES += cetron_ct3003
 
+define Device/clt_r30b1
+  DEVICE_VENDOR := CLT
+  DEVICE_MODEL := R30B1
+  DEVICE_DTS := mt7981b-clt-r30b1
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += clt,r30b1
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += clt_r30b1
+
+define Device/clt_r30b1-112m
+  DEVICE_VENDOR := CLT
+  DEVICE_MODEL := R30B1
+  DEVICE_VARIANT := (with 112M ubi)
+  DEVICE_DTS := mt7981b-clt-r30b1-112m
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += clt,r30b1
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += clt_r30b1-112m
+
 define Device/cetron_ct3003-ubootmod
   DEVICE_VENDOR := Cetron
   DEVICE_MODEL := CT3003
